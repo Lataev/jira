a6cc7579b2a6ee0323c86050f29df09ee86f0485
Lataev <postavegas@gmail.com>
2025-07-21 16:39:05 +0700

    Условия по пользователю

---
+++ conditions/cond.groovy
@@ -0,0 +1,28 @@
+'''
+Если пользователь не участник группы group и ( исполнитель или имеет роль Диспетчер), переход разрешён.
+'''
+
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.security.roles.ProjectRoleManager
+ 
+def assignee = issue.assignee
+
+def projectRoleManager = ComponentAccessor.getComponent(ProjectRoleManager)
+def devProjectRole = projectRoleManager.getProjectRole("Диспетчер")
+
+if (
+        (
+            projectRoleManager.isUserInProjectRole(currentUser, devProjectRole, issue.getProjectObject()) 
+        || 
+            assignee == currentUser
+        ) 
+    &&
+        !currentUser.isMemberOfGroup('group')
+    )
+{
+    return true
+} 
+else 
+{
+    return false
+}
