8b27de978e468c883fc4d06575fbaca5b72024f0
Lataev <postavegas@gmail.com>
2025-07-31 13:15:02 +0700

    rename

--- workflows/add_approver_by_issue_condition.groovy
+++ workflows/add_approver_by_issue_condition.groovy
@@ -1,5 +1,5 @@
 '''
-Если id Тип запроса клиента = 123, если в поле выбора 16000 выбрана опция 15000, админ устанавливает в поле согласующего пользователя user.
+Если id Тип запроса клиента = 123 и в поле выбора 16000 выбрана опция 15000, админ устанавливает в поле согласующего пользователя user.
 '''
 
 def requestTypeID = issue.requestType.id
@@ -7,7 +7,7 @@ if ( issue.requestType.id == 123) {
     if (issue.getCustomFieldValue(16000).optionId == 15000) {
         Users.runAs('admin') {
             issue.set {
-                setCustomFieldValue('Согласующий', 'user')
+                setCustomFieldValue('Согласующий', 'user_login')
             }
         }
     }
