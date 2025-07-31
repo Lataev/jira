3babf0373c84baffaccb823368bd325912d5d7c5
Lataev <postavegas@gmail.com>
2025-07-31 14:46:35 +0700

    Консольная команда

---
+++ workflows/run_cons_comm.groovy
@@ -0,0 +1,16 @@
+"""
+Запуск консольной команды на сервере Jira (Ubuntu).
+Здесь запускается python скрипт, который принимает аргумент в виде ключа задачи.
+"""
+
+def processBuilder = new ProcessBuilder(
+    "/usr/bin/python3.10",
+    "/home/user/scripts/python_script.py",
+    issue.key
+)
+
+processBuilder.redirectErrorStream(true)
+def process = processBuilder.start()
+
+def output = process.inputStream.text
+log.warn(output)
