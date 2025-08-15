94dd69b63bc758e2258702115f616e48eaa2b868
Lataev <postavegas@gmail.com>
2025-08-15 15:40:18 +0700

    ad_user_attributes_by_ad_connection

---
+++ console/ad_user_attributes_by_ad_connection.groovy
@@ -0,0 +1,30 @@
+'''
+Получение значений атрибутов пользователя AD непосредственно из AD
+'''
+
+import com.onresolve.scriptrunner.ldap.LdapUtil
+import org.springframework.ldap.core.AttributesMapper
+import javax.naming.directory.SearchControls
+
+
+if (issue.getCustomFieldValue('Пользователь')) {
+
+    def attributes = ['cn', 'title', 'department', 'company']
+    def user = [:]
+
+    LdapUtil.withTemplate("AD") { template ->
+
+        template.search("", "(sAMAccountName=*$jira_user.name*)", SearchControls.SUBTREE_SCOPE, { attributes -> 
+
+            for (attribute in attributes) {
+                if (attributes.get(attribute)) {
+                    user[attribute] = attributes.get(attribute).get().toString()
+                }
+            }
+        
+        } as AttributesMapper<Object>)
+    }
+
+    log.warn(user)
+
+}
