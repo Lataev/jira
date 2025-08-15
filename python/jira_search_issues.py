33f92d6a37a764ccbdf60a1417253fb5d5ca0f04
Lataev <postavegas@gmail.com>
2025-08-15 16:13:03 +0700

    jira_search_issues.py

---
+++ python/jira_search_issues.py
@@ -0,0 +1,39 @@
+'''
+Простой поиск запросов в Jira
+'''
+
+from jira import JIRA, client
+from secret import auth_data
+
+
+def search_jira_issues(jql_query) -> client.ResultList:
+
+    # Аутентификация по логину-паролю, либо по токену
+    jira = JIRA(
+        basic_auth=(
+            auth_data.jira_log_pass().login,
+            auth_data.jira_log_pass().password,
+        ),
+        # token_auth=jira_log_pass().token,
+        options={"server": auth_data.jira_log_pass().server, "verify": False},
+    )
+
+    issues = jira.search_issues(jql_query)
+    jira.close()
+
+    return issues
+
+
+if __name__ == '__main__':
+
+    # jql фильтр
+    jql_query = '''
+                project = proj
+                AND 
+                "Тип запроса клиента" = Доступ
+                AND 
+                status in ( "В работе", Открыто )
+                '''
+
+    issues = search_jira_issues(jql_query)
+    print(f"Найдено запросов: {issues.total}")
