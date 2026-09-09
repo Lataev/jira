7eaf6400cd75635c04872ca14f842ba7f633b1d1
Латаев Дмитрий Леонидович <Lataev.d.l@new-mmc.com>
2026-09-09 10:44:34 +0700

    group.members

--- jobs/add_users_in_group.groovy
+++ jobs/add_users_in_group.groovy
@@ -6,6 +6,7 @@ import com.atlassian.jira.component.ComponentAccessor
 
 def userManager = ComponentAccessor.getUserManager()
 def allUsers = userManager.getAllApplicationUsers() // функция считается устаревшей, но работает
+def allUsers2 = Groups.getByName('jira-software-users').members // можно использовать участников самой массовой группы
 
 def group = Groups.getByName("groupName")
 
